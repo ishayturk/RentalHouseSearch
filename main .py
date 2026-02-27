be7b287b2eabeb99045a2cc3b935c86864ccd5d4
ishayturk <130680079+ishayturk@users.noreply.github.com>
2026-02-27 21:34:38 +0200

    Update main .py

--- main .py
+++ main .py
@@ -1,10 +1,11 @@
+import streamlit as st
 from openai import OpenAI
 
-client = OpenAI(api_key="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")
+client = OpenAI(api_key=st.secrets["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"])
 
-response = client.chat.completions.create(
+response = client.responses.create(
     model="gpt-4o-mini",
-    messages=[{"role": "user", "content": "תגיד שלום"}],
+    input="תגיד שלום"
 )
 
-print(response.choices[0].message.content)
+st.write(response.output_text)
