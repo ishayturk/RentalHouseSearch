4cdd518f07c9762499251049f25f571401cde37e
ishayturk <130680079+ishayturk@users.noreply.github.com>
2026-02-27 21:36:29 +0200

    Update main .py

--- main .py
+++ main .py
@@ -1,7 +1,7 @@
 import streamlit as st
 from openai import OpenAI
 
-client = OpenAI(api_key=st.secrets["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"])
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 response = client.responses.create(
     model="gpt-4o-mini",
