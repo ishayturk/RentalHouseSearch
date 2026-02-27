0d2aa89ba1280b6b4d731e4d7835d44cc916aa5f
ishayturk <130680079+ishayturk@users.noreply.github.com>
2026-02-27 20:54:10 +0200

    Update main .py

--- main .py
+++ main .py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 
-client = OpenAI(api_key="הדבק_כאן_את_ה_key_שלך")
+client = OpenAI(api_key="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")
 
 response = client.chat.completions.create(
     model="gpt-4o-mini",
